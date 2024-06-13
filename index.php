82df66f3185d30a3584fa560af7d74d84ad415cd
Nicholas Alvin <mystm6219@gmail.com>
2024-06-13 21:59:19 +0700

    index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Pendaftaran siswa smk 1</title>
+    <title>Pendaftaran siswa smk 1 jakarta</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <link rel="stylesheet" href="style (2).css"  >
 
@@ -58,7 +58,7 @@
                 style="margin-right: .75rem;"><i class="fas fa-bicycle"></i></a>
             </div>
             <div class="bg-white px-4 pb-4 pt-3-5">
-              <h5 class="card-title h5 living-coral-text">MIS JERUKSARI</h5>
+              <h5 class="card-title h5 living-coral-text">SMK 1 JAKARTA</h5>
               <div class="d-flex justify-content-between living-coral-text">
                 <h6 class="card-subtitle font-weight-light">SMK 1 JAKARTA</h6>
               </div>
