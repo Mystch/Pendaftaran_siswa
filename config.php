1ffec8002810f3bf35e9468d07d937d949102aa4
Nicholas Alvin <mystm6219@gmail.com>
2024-06-13 19:41:30 +0700

    Add files via upload

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+$server = "localhost";
+$user = "root";
+$password ="";
+$nama_databases="wajiih_pplg2";
+
+$db= mysqli_connect($server,$user,$password,$nama_databases);
+
+if(!$db ){
+    die("Gagal terhubung dengan databases:".mysqli_connect_eror());
+}
